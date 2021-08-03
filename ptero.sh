82ffea983782adcc2d6977ba6e0dadb387c42841
XmomoooDe <86575068+XmomoooDe@users.noreply.github.com>
2021-08-03 06:42:46 +0200

    Update ptero.sh

--- ptero.sh
+++ ptero.sh
@@ -68,15 +68,15 @@ if [ $choice == "8" ]
     then
 curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
 
-chmod -R 755 storage/* bootstrap/cache
 
-composer install --no-dev --optimize-autoloader
 
-php artisan view:clear && php artisan config:clear
 
-php artisan migrate --seed --force
 
-chown -R www-data:www-data /var/www/pterodactyl/*
+
+
+
+
+
 
 echo "Successfully updated"
 
